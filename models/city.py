3c2042c319bff4d72bd303cda0e8ae442ffa9930
Frank Anin <frankanin99@gmail.com>
2024-03-24 20:57:18 +0000

    AirBnB console

--- models/city.py
+++ models/city.py
@@ -4,7 +4,12 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Defines the City class."""
+    """Defines the City class.
+
+    Attributes:
+        state_id (str): state id
+        name (str): name of the city
+    """
 
     state_id = ""
     name = ""
--- models/review.py
+++ models/review.py
@@ -4,7 +4,13 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Defines the Review class."""
+    """Defines the Review class.
+
+    Attributes:
+        place_id (str): Place id.
+        user_id (str): User id.
+        text (str): text of the review.
+    """
 
     place_id = ""
     user_id = ""
