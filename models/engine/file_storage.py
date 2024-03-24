f11d34a74aa8a7cde742fcc677989a095cb53894
Frank Anin <frankanin99@gmail.com>
2024-03-24 21:14:55 +0000

    AirBnB console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,34 +11,34 @@ from models.review import Review
 
 
 class FileStorage:
-    """This class serializes/deserializes instances a JSON file"""
+    """Defines the FileStorage class."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
-        odict = FileStorage.__objects
-        obj_dict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        """Serialize __objects to the JSON file __file_path."""
+        fs_dict = FileStorage.__objects
+        obj_dict = {obj: fs_dict[obj].to_dict() for obj in fs_dict.keys()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """"Deserializes the JSON file to __objects."""
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
                 obj_dict = json.load(f)
-                for obj in obj_dict.values():
-                    cls_name = obj["__class__"]
-                    del obj["__class__"]
-                    self.new(eval(cls_name)(**obj))
+                for i in obj_dict.values():
+                    cls_name = i["__class__"]
+                    del i["__class__"]
+                    self.new(eval(cls_name)(**i))
         except FileNotFoundError:
             return
